ee30cd0a49d9ed3f8a30b0fe219d3b75c5447404
Sk8rSeth <sethhowell2197@gmail.com>
2021-08-04 22:22:31 -0700

    website v1.0

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <img class="logo" alt="Space Castle" src="./assets/SC-Logo.png" />
+  <SpaceCastle />
+</template>
+
+<script setup>
+import SpaceCastle from './components/SpaceCastle.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+html {
+    height: 100%;
+}
+body {
+    margin: 0;
+    padding: 0;
+    min-height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #eee;
+  background-image: url('./assets/BG.jpg');
+  background-position: top left;
+  background-repeat: no-repeat;
+  padding-bottom: 1rem;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+
+#app img.logo {
+    margin-top: 5rem;
+    height: 25rem;
+}
+
+#app h4 {
+    width: 50%;
+    margin: 2rem auto;
+}
+</style>
---
+++ src/components/SpaceCastle.vue
@@ -0,0 +1,78 @@
+<template>
+  <h4>A podcast where three nerds (Alex, D.T. and Seth) hurtle through space, debating movies, books, games, and answering YOUR pop culture questions... all to maintain their own sanity!</h4>
+
+  <p>
+    <a href="https://www.youtube.com/channel/UCL9LooNWHVGILb5fB4JJvXQ/featured" target="_blank">
+        <img src="../assets/YoutubeBadge.png" alt="">
+    </a>
+    <a href="https://open.spotify.com/show/0qeF4snFUsQYgjzmZeTGs8?si=Nmfh_1RPSWaX1GOagCn01Q&dl_branch=1" target="_blank">
+        <img src="../assets/SpotifyBadge.png" alt="">
+    </a>
+    <a href="https://podcasts.apple.com/us/podcast/space-castle/id1499764926" target="_blank">
+        <img src="../assets/AppleBadge.png" alt="">
+    </a>
+    <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9sZXZlbHVweW91cmZhbmRvbS5jb20vZmVlZC9wb2RjYXN0L3NwYWNlLWNhc3RsZQ" target="_blank">
+        <img src="../assets/GoogleBadge.png" alt="">
+    </a>
+  </p>
+  <p class="schedule">
+    New Episode Every Wednesday
+    <div>(Hopefully)</div>
+</p>
+  <div class="bottom-stuff">
+      <div class="logos">
+          <a href="https://twitter.com/spacecastlepod" target="_blank">
+              <img src="../assets/Twitter.png" alt="">
+          </a>
+          <a href="https://levelupyourfandom.com" target="_blank">
+              <img src="../assets/LUYF.png" alt="">
+          </a>
+          <!-- <a href="https://www.youtube.com/channel/UCL9LooNWHVGILb5fB4JJvXQ/featured" target="_blank">
+              <img src="../assets/YoutubeBadge.png" alt="">
+          </a> -->
+      </div>
+      <div class="contact">
+          <p>&copy; 2021 Space Castle</p>
+          <a href="mailto:spcecastlepodcast@gmail.com">Email Us</a> | <a href="#">Privacy Statement</a>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+
+}
+
+img {
+    height: 3rem;
+}
+
+.bottom-stuff {
+    margin-top: 3rem;
+}
+
+.bottom-stuff a {
+    margin: 1rem;
+    color: #fff;
+}
+
+.bottom-stuff p {
+    margin: 0;
+    padding: 0;
+}
+
+.bottom-stuff .contact {
+    font-size: .75rem;
+}
+
+p.schedule {
+    margin: 0;
+}
+</style>
